54f04e84228a76ae614e7f58207b822bb3bb4e01
ethan <ethansmitty@gmail.com>
2019-03-12 21:39:08 -0400

    Added dedicated placeholder L3 activity

---
+++ app/src/main/java/org/sunyulstercs/supportsmeapp/InfoDetailActivity.java
@@ -0,0 +1,64 @@
+package org.sunyulstercs.supportsmeapp;
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Placeholder class, uses an old version of InfoActivity as a way to display the details. Pending an actual design, this will be removed.
+ */
+public class InfoDetailActivity extends AppCompatActivity
+{
+    private String[] listItems;
+    private String [][] catData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_arrow_back));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        Bundle extras = getIntent().getExtras(); //Get data passed to activity with Intent
+        if (extras != null)
+        {
+            listItems = extras.getStringArray("ListItems"); //ListItems is a String array which will be displayed in RecyclerView
+            Object[] objectArray = (Object[]) extras.getSerializable("CategoryData");
+            if(objectArray!=null) {
+                catData = new String[objectArray.length][];
+                for (int i = 0; i < objectArray.length; i++) {
+                    catData[i] = (String[]) objectArray[i];
+                }
+            }
+
+            try
+            {
+                getSupportActionBar().setTitle(extras.getString("ActivityName")); //Setting toolbar title based on which category this is
+            } catch (NullPointerException e)
+            {
+                Log.e(this.getLocalClassName(), "Null title");
+            }
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.infoListView);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        RecyclerView.Adapter adapter = new InfoRecyclerAdapter(listItems, catData, getApplicationContext());
+        recyclerView.setAdapter(adapter);
+    }
+}
