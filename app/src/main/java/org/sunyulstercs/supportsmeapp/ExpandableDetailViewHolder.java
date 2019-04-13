7bf4d3c4d1321ef34ec37cbcc12fe7f34a13b8a5
ethan <ethansmitty@gmail.com>
2019-04-12 20:59:32 -0400

    Added comment

--- app/src/main/java/org/sunyulstercs/supportsmeapp/ExpandableDetailViewHolder.java
+++ app/src/main/java/org/sunyulstercs/supportsmeapp/ExpandableDetailViewHolder.java
@@ -7,19 +7,17 @@ import com.thoughtbot.expandablerecyclerview.viewholders.ChildViewHolder;
 
 class ExpandableDetailViewHolder extends ChildViewHolder
 {
-    //private TextView infoTitle;
+    //TODO: This is where you add new stuff, like "More Info" buttons
     private TextView infoDesc;
 
     ExpandableDetailViewHolder(View view)
     {
         super(view);
-        //infoTitle = view.findViewById(R.id.info_title);
         infoDesc = view.findViewById(R.id.info_desc);
     }
 
     void OnBind(InfoItem info)
     {
-        //infoTitle.setText(info.getTitle());
         infoDesc.setText(info.getDesc());
 
     }
