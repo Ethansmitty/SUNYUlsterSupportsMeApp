006b9f9025c36eae4b284cbad52d668068114234
ethan <ethansmitty@gmail.com>
2019-05-02 22:52:06 -0400

    Added nav fragment and banner image at top of screen for Calendar layouts

--- app/src/main/java/org/sunyulstercs/supportsmeapp/navFragment.java
+++ app/src/main/java/org/sunyulstercs/supportsmeapp/navFragment.java
@@ -18,72 +18,75 @@ import android.widget.ImageButton;
  */
 public class navFragment extends Fragment implements View.OnClickListener {
 
-    private ImageButton homeButton, profileButton, locationButton;
-    public navFragment() {
-        // Required empty public constructor
-    }
+	private ImageButton homeButton, profileButton, locationButton;
+	public navFragment() {
+		// Required empty public constructor
+	}
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment navFragment.
-     */
-    public static navFragment newInstance() {
-        return new navFragment();
-    }
+	/**
+	 * Use this factory method to create a new instance of
+	 * this fragment using the provided parameters.
+	 *
+	 * @return A new instance of fragment navFragment.
+	 */
+	public static navFragment newInstance() {
+		return new navFragment();
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    }
+	}
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_nav, container, false);
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		// Inflate the layout for this fragment
+		View view = inflater.inflate(R.layout.fragment_nav, container, false);
 
-        homeButton = view.findViewById(R.id.homeButton);
-        locationButton = view.findViewById(R.id.locationButton);
-        profileButton = view.findViewById(R.id.profileButton);
+		homeButton = view.findViewById(R.id.homeButton);
+		locationButton = view.findViewById(R.id.locationButton);
+		profileButton = view.findViewById(R.id.profileButton);
 
-        homeButton.setOnClickListener(this);
-        locationButton.setOnClickListener(this);
-        profileButton.setOnClickListener(this);
+		homeButton.setOnClickListener(this);
+		locationButton.setOnClickListener(this);
+		profileButton.setOnClickListener(this);
 
-        return view;
-    }
+		return view;
+	}
 
-    @Override
-    public void onClick(View view)
-    {
-        switch (view.getId())
-        {
-            case R.id.homeButton :
-                if (!(getActivity() instanceof MainActivity))  //If we're already nav_home, do nothing
-                {
-                    Intent homeIntent = new Intent(this.getContext(), MainActivity.class);
-                    homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //Clears stack when going nav_home
-                    startActivity(homeIntent);
+	@Override
+	public void onClick(View view)
+	{
+		switch (view.getId())
+		{
+			case R.id.homeButton :
+				if (!(getActivity() instanceof MainActivity))  //If we're already nav_home, do nothing
+				{
+					Intent homeIntent = new Intent(this.getContext(), MainActivity.class);
+					homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //Clears stack when going nav_home
+					startActivity(homeIntent);
+				}
+
+				break;
+
+			case R.id.locationButton :
+				Intent mapIntent = new Intent(this.getContext(), MapActivity.class);
+				mapIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT); //If activity is in the stack somewhere, reuse it.
+				startActivity(mapIntent);
+				break;
+
+            case R.id.profileButton :
+				if (!(getActivity() instanceof CalendarMain))
+				{
+                    Intent calIntent = new Intent(this.getContext(), CalendarMain.class);
+                    calIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                    startActivity(calIntent);
                 }
+				break;
 
-                break;
-
-            case R.id.locationButton :
-                Intent mapIntent = new Intent(this.getContext(), MapActivity.class);
-                mapIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT); //If activity is in the stack somewhere, reuse it.
-                startActivity(mapIntent);
-                break;
-
-            case R.id.profileButton : {
-                Intent homeIntent = new Intent(this.getContext(), CalendarMain.class);
-                homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //Clears stack when going home
-                startActivity(homeIntent);
-                break;
-            }
-        }
-    }
+		}
+	}
 
 }
